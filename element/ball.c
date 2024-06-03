1e9a6b4c363f8f0a8b4e21ad2ca7b5ce675eb2eb
Xixun31 <bryant940214@gmail.com>
2024-06-03 22:26:15 +0800

    ball practice 1

---
+++ element/ball.c
@@ -0,0 +1,17 @@
+#include "Ball.h"
+
+Elements *New_Ball(int label){
+
+}
+void Ball_update(Elements *self){
+
+}
+void Ball_interact(Elements *self, Elements *tar){
+
+}
+void Ball_draw(Elements *self){
+
+}
+void Ball_destory(Elements *self){
+
+}
---
+++ element/ball.h
@@ -0,0 +1,21 @@
+#ifndef BALL_H_INCLUDED
+#define BALL_H_INCLUDED
+#include "element.h"
+#include "../scene/gamescene.h" // for element label
+#include "../shapes/Shape.h"
+/*
+   [tree object]
+*/
+typedef struct _Ball
+{
+    int x, y;          // the position of image
+    int r;  //radius
+    Shape *hitbox; // the hitbox of object
+} Ball;
+Elements *New_Ball(int label);
+void Ball_update(Elements *self);
+void Ball_interact(Elements *self, Elements *tar);
+void Ball_draw(Elements *self);
+void Ball_destory(Elements *self);
+
+#endif
