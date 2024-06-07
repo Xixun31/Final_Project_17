cff296d038da2096c670f22d259a2925dd54821e
Xixun31 <bryant940214@gmail.com>
2024-06-07 12:38:49 +0800

    box 1

--- element/box.c
+++ element/box.c
@@ -3,54 +3,61 @@
 #include "../shapes/Shape.h"
 #include "../global.h"
 #include <allegro5/allegro_primitives.h>
-#include "nball.h"
-
-Elements *New_Box(int label) {
+/*
+   [Box function]
+*/
+Elements *New_Box(int label)
+{
     Box *pDerivedObj = (Box *)malloc(sizeof(Box));
     Elements *pObj = New_Elements(label);
-
-    // Initialize Box properties
-    pDerivedObj->x = WIDTH - 50 ;
-    pDerivedObj->y = HEIGHT - 50;
-    pDerivedObj->w = 50;
-    pDerivedObj->h = 50;
-    pDerivedObj->c = al_map_rgb(205, 205, 205);
-    pDerivedObj->hitbox = New_Rectangle(pDerivedObj->x, pDerivedObj->y, pDerivedObj->x + pDerivedObj->w,
+    // setting derived object member
+    pDerivedObj->x = 0;//WIDTH - 75;
+    pDerivedObj->y = 0;
+    pDerivedObj->w = WIDTH;//75;
+    pDerivedObj->h = 75;
+    pDerivedObj->c = al_map_rgb(255, 255, 255);
+    pDerivedObj->hitbox[0] = New_Rectangle(pDerivedObj->x, pDerivedObj->y, pDerivedObj->x + 5,
                                         pDerivedObj->y + pDerivedObj->h);
+    pDerivedObj->hitbox[1] = New_Rectangle(pDerivedObj->x + pDerivedObj->w - 5, pDerivedObj->y, 
+                                        pDerivedObj->x + pDerivedObj->w, pDerivedObj->y + pDerivedObj->h);
+    pDerivedObj->hitbox[2] = New_Rectangle(pDerivedObj->x, pDerivedObj->y + pDerivedObj->h - 5, 
+                                        pDerivedObj->x + pDerivedObj->w, pDerivedObj->y + pDerivedObj->h);
+    pDerivedObj->hitbox[3] = New_Rectangle(pDerivedObj->x, pDerivedObj->y, 
+                                        pDerivedObj->x + pDerivedObj->w, pDerivedObj->y + 5);
 
     pObj->inter_obj[pObj->inter_len++] = Nball_L;
 
+    // setting derived object function
     pObj->pDerivedObj = pDerivedObj;
     pObj->Update = Box_update;
+    pObj->Interact = Box_interact;
     pObj->Draw = Box_draw;
-    pObj->Destroy = Box_destroy;
-
+    pObj->Destroy = Box_destory;
     return pObj;
 }
-
-void Box_update(Elements *const ele) {
-
-}
-void Box_interact(Elements *self, Elements *tar){
+void Box_update(Elements *self) {}
+void Box_interact(Elements *self, Elements *tar) {
     Box *Obj = ((Box *)(self->pDerivedObj));
     if (tar->label == Nball_L)
     {
-        Nball *ba = ((Nball *)(tar->pDerivedObj));
-        if (ba->hitbox->overlap(ba->hitbox, Obj->hitbox))
+        Nball *nb = ((Nball *)(tar->pDerivedObj));
+        if (nb->hitbox->overlap(nb->hitbox, Obj->hitbox[0] || Obj->hitbox[1] || Obj->hitbox[2] || Obj->hitbox[3]))
         {
-            self->dele = true;
+            Obj->c = al_map_rgb(55, 255, 255);
+        }else{
+            Obj->c = al_map_rgb(255, 255, 255);
         }
     }
 }
-void Box_draw(Elements *const ele) {
-    Box *Obj = ((Box *)(ele->pDerivedObj));
+void Box_draw(Elements *self)
+{
+    Box *Obj = ((Box *)(self->pDerivedObj));
     al_draw_filled_rectangle(Obj->x, Obj->y, Obj->x + Obj->w, Obj->y + Obj->h, Obj->c);
 }
-
-void Box_destroy(Elements *const ele) {
-    Box *Obj = ((Box *)(ele->pDerivedObj));
+void Box_destory(Elements *self)
+{
+    Box *Obj = ((Box *)(self->pDerivedObj));
     free(Obj->hitbox);
     free(Obj);
-    free(ele);
-    // random call tool
+    free(self);
 }
--- element/box.h
+++ element/box.h
@@ -1,18 +1,21 @@
 #ifndef Box_H_INCLUDED
 #define Box_H_INCLUDED
-
-#include "../shapes/Rectangle.h"
 #include "element.h"
-
-typedef struct Box {
+#include "../scene/gamescene.h" // for element label
+#include "../shapes/Shape.h"
+/*
+   [Box object]
+*/
+typedef struct _Box
+{
     float x, y, w, h;
     ALLEGRO_COLOR c;
-    Shape *hitbox;
+    Shape *hitbox[4];
 } Box;
-
 Elements *New_Box(int label);
-void Box_update(Elements *const ele);
-void Box_draw(Elements *const ele);
-void Box_destroy(Elements *const ele);
+void Box_update(Elements *self);
+void Box_interact(Elements *self, Elements *tar);
+void Box_draw(Elements *self);
+void Box_destory(Elements *self);
 
 #endif
