632b7068831bd6b847f8d2e7498b0247be78459d
ellina-tsao <yunhsuntsao@gmail.com>
2024-06-10 22:45:20 +0800

    meny background

--- scene/menu.h
+++ scene/menu.h
@@ -6,17 +6,21 @@
 #include <allegro5/allegro_acodec.h>
 #include <allegro5/allegro_font.h>
 #include <allegro5/allegro_ttf.h>
+#include <allegro5/allegro_image.h> 
+
 /*
    [Menu object]
 */
 typedef struct _Menu
 {
     ALLEGRO_FONT *font;
+    ALLEGRO_BITMAP *background; 
     ALLEGRO_SAMPLE *song;
     ALLEGRO_SAMPLE_INSTANCE *sample_instance;
     int title_x, title_y;
     bool mouse_over_new_game, mouse_over_level, mouse_over_about;
 } Menu;
+
 Scene *New_Menu(int label);
 void menu_update(Scene *self);
 void menu_draw(Scene *self);
